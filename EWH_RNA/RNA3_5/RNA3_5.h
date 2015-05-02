85b9abae84c467ced7fec92aeec5061135654eed
kbuzzy <kbuzzy@users.noreply.github.com>
2015-05-02 12:28:12 -0700

    Corrected Pins for Motors

--- EWH_RNA/RNA3_5/RNA3_5.h
+++ EWH_RNA/RNA3_5/RNA3_5.h
@@ -57,9 +57,9 @@ int pausePin=24;
 int unpausePin=22;
 
 int solenoidPins[]= {1, 2};
-int IRPins[] = {43, 42};
-int washPins[] = {35, 34};
-int elutionPins[] = {33, 32};
+int IRPins[] = {33, 32};
+int washPins[] = {43, 42};
+int elutionPins[] = {35, 34};
 int sterilizationPins[] = {43, 42};
 int ignore[] = {-1, -1};
 
